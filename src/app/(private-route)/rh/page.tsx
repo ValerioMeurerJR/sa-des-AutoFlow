657ec7a16a798c148d2fde5d3476c120265fb395
Valerio Meurer <valerio.meurer_jt@live.com>
2025-04-24 19:05:46 -0300

    SA

--- src/app/(private-route)/rh/page.tsx
+++ src/app/(private-route)/rh/page.tsx
@@ -5,7 +5,10 @@ import { ButtonCustom } from "@/components/ButtonCustom";
 import { InputCustom } from "@/components/InputCustom";
 import axios from "axios";
 import { v4 as uuid } from "uuid";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import SkeletonTable from "@/components/SkeletonTable";
+import DataTable from "@/components/DataTable";
+import { GridColDef } from "@mui/x-data-grid";
 
 const host = 'http://localhost:63977';
 
@@ -24,12 +27,36 @@ type User = {
     createDate: string;
 }
 
+
+    const columns: GridColDef[] = [
+        { field: 'nome', headerName: 'Nome', width: 200 },
+        { field: 'cpf', headerName: 'CPF', width: 200 },
+        { field: 'cargo', headerName: 'Cargo', width: 200 },
+        { field: 'login', headerName: 'Login', width: 200 }
+    ];
+
 export default function Rh() {
     const [nome, setNome] = useState<string>("");
     const [cpf, setCpf] = useState<string>("");
     const [cargo, setCargo] = useState<string>("");
     const [login, setLogin] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [listaUser, setlistaUser] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const paginationModel = { page: 0, pageSize: 6 };
+
+    useEffect(() => {
+        loadList();
+    }, []);
+
+    async function loadList() {
+        const response = await axios.get(`${host}/ListaUsuario`);
+        const ListCompletoSort = response.data?.sort((a: any, b: any) => (
+            new Date(b.createDate).getTime() - new Date(a.createDate).getTime()
+        ));
+        setlistaUser(ListCompletoSort);
+        setLoading(false);
+    }
 
     async function handleRegister(event: FormEvent) {
         event.preventDefault();
@@ -37,6 +64,7 @@ export default function Rh() {
             alert("Preencha todos os campos!");
             return;
         }
+        
 
         const user: User = {
             "id": uuid(),
@@ -54,16 +82,16 @@ export default function Rh() {
         setCargo("")
         setLogin("")
         setPassword("");
-
+        loadList()
 
     }
 
 
     return (
         <div className="body-rh">
-            <h1>Produção de Carro</h1>
+            <h1>RH</h1>
             <main className="main-rh">
-                <h2>Cadastro novo veiculo</h2>
+                <h2>Cadastro de Funcionarios</h2>
                 <form className="form-rh">
                     <InputCustom texto="Nome" type="text" message={nome} setMessage={setNome} />
                     <InputCustom texto="CPF" type="text" message={cpf} setMessage={setCpf} />
@@ -76,13 +104,13 @@ export default function Rh() {
                 </form>
             </main>
             <div className="lista-quality">
-                {/* {loading ? (
+                {loading ? (
                     <SkeletonTable width={150} />
-                ) : listCarState.length > 0 ? (
-                    <DataTable columns={columns} dataLista={listCarState} paginationModel={paginationModel} />
+                ) : listaUser.length > 0 ? (
+                    <DataTable columns={columns} dataLista={listaUser} paginationModel={paginationModel} />
                 ) : (
                     <p>Nenhum dado encontrado.</p>
-                )} */}
+                )}
             </div>
         </div>
     );
